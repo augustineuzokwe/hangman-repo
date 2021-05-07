5fd66791bd603e2cb3dfda2fab49264e7ae42926
Augustine Uzokwe <augustine.uzokwe@gmail.com>
2021-05-07 20:37:21 +0200

    add skeleton proj
    test commit

---
+++ Hangman/Hangman/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hangman
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("test commit and push");
+        }
+    }
+}
