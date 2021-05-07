f6896c5c3c398a635695cbdd8fb51d65e4000137
Augustine Uzokwe <augustine.uzokwe@gmail.com>
2021-05-07 19:54:19 +0200

    push empty program

---
+++ quizmaker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace quizmaker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! 1");
+        }
+    }
+}
