79ded00219a3d6d3659911636ac34d725081702f
Augustine Uzokwe <augustine.uzokwe@gmail.com>
2021-05-11 22:24:57 +0200

    add method to return random word

---
+++ Hangman/Hangman/RandomWord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Hangman
+{
+    public class RandomWord
+    {
+        public string getRandomWord()
+        {
+            Random randomNumber = new Random();
+
+            int number = randomNumber.Next(1, Data.words.Length);
+
+            string randomWord = "";
+
+            switch (number)
+            {
+                case 1:
+                    randomWord = Data.words[number];
+                    break;
+
+                case 2:
+                    randomWord = Data.words[number];
+                    break;
+
+                case 3:
+                    randomWord = Data.words[number];
+                    break;
+
+                case 4:
+                    randomWord = Data.words[number];
+                    break;
+            }
+
+            return randomWord;
+        }
+    }
+}
