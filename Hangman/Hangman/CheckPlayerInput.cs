1b0dfc783d5adbf179b156a5cb1f126e84d20de6
Augustine Uzokwe <augustine.uzokwe@gmail.com>
2021-05-11 22:20:28 +0200

    add method to check players input

---
+++ Hangman/Hangman/CheckPlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Hangman
+{
+    public class CheckPlayerInput
+    {
+        public static Boolean doesPlayerInputExist(string selectedWord, char playerInput)
+        {
+            if (selectedWord.Contains(playerInput.ToString()))
+                {
+                    return true;
+                }
+            
+            return false;
+        }
+    }
+}
