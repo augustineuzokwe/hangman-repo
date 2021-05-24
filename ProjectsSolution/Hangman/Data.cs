ec4b1e2f34020df6ed7f1909c7e69b3af9c8ede5
Augustine Uzokwe <augustine.uzokwe@gmail.com>
2021-05-24 20:04:28 +0200

    renamed solution directory

---
+++ ProjectsSolution/Hangman/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Hangman
+{
+    public static class Data
+    {
+        public static List<string> GetWords()
+        {
+            return new List<string> { "street", "storing", "shopping", "buying" };
+        }
+
+        public static string GetWord()
+        {
+            Random randomNumber = new Random();
+            var wordList = GetWords();
+            int number = randomNumber.Next(wordList.Count);
+
+            return wordList[number];
+        }
+    }
+}
---
+++ ProjectsSolution/Hangman/ScrambleWord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Hangman
+{
+    public class ScrambleWord
+    {
+
+        private static readonly Random RandomNumberGenerator = new Random();
+
+        public static string GetScrambledWord(string selectedWord)
+        {
+            return selectedWord
+                    .Replace(selectedWord[RandomNumberGenerator.Next(selectedWord.Length)], '_')
+                    .Replace(selectedWord[RandomNumberGenerator.Next(selectedWord.Length)], '_')
+                    .Replace(selectedWord[RandomNumberGenerator.Next(selectedWord.Length)], '_');
+        }
+    }
+}
